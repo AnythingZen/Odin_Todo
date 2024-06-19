f7b87d6b386d94d07371e06c667942c6cd1b3adf
anythingzen <chengzhanxd@gmail.com>
2024-06-20 01:49:31 +0800

    update for testing addWork button

--- src/main.js
+++ src/main.js
@@ -4,3 +4,21 @@ import { topBarDOM, sideBarDOM, } from './DOMContent.js';
 const hero = document.querySelector('.hero')
 topBarDOM(hero)
 sideBarDOM(hero)
+
+import { createPopUpTodo } from "./DOMContent.js";
+import { Todo } from "./classes.js";
+
+// Create workspace
+
+// Add todos
+const toDoContainer = document.createElement('div')
+toDoContainer.className = 'toDoContainer'
+hero.appendChild(toDoContainer)
+
+const addTodo = document.querySelector('.sideBarAddWork')
+addTodo.addEventListener('click', () => {
+    createPopUpTodo(hero, (formProps) => {
+        const newTodo = new Todo(formProps)
+        newTodo.appendToProject(toDoContainer)
+    })
+})
